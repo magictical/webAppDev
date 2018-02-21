6009efa717077c498b6eced4ec9d96979331624a
Ian Kim <magicticaldev@gmail.com>
2018-02-21 10:13:34 +0000

    Add home.ejs - use render()

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send("Welcome to the home Page!");
+    // res.send("Welcome to the home Page!");
+    // change to render
+    res.render("home.ejs");
 })
 
 app.listen(process.env.PORT, process.env.IP, function() {
