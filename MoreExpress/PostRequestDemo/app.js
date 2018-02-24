172324f31f6869e8f99a3e6a2e5ab51acce88f9e
Ian Kim <magicticaldev@gmail.com>
2018-02-24 16:22:14 +0000

    Add friends name from friends.ejs at its <li>

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // send, render에서 ejs 파일 불러올때 확장자명 생략하는 설정
 app.set("view engine", "ejs");
 
+var friends = ["Tony", "Murray", "Kim", "Endo", "Julia"];
 
 // set default route
 app.get("/", function(req, res) {
@@ -17,13 +18,12 @@ app.get("/", function(req, res) {
 
 // add route for addfriends - this will be the route for postRequest
 app.post("/addfriends", function(req, res) {
-    console.log(req.body);
+    friends.push(req.body.newfriend);
     res.send("You are at /addfriends route!!");
 })
 
 // add frends list
 app.get("/friends", function(req, res) {
-    var friends = ["Tony", "Murray", "Kim", "Endo", "Julia"];
     res.render("friends",{friends:friends});
 })
 
