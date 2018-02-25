e4d47be7a886d5d65aef2ff35d88475b44915e3f
Ian Kim <magicticaldev@gmail.com>
2018-02-25 05:01:07 +0000

    Add root route which connected to search.ejs by render()

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -4,6 +4,11 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+// add root route
+app.get("/", function(req, res) {
+    res.render("search");
+})
+
 app.get("/results", function(req, res) {
     request('http://www.omdbapi.com/?s=star&apikey=thewdb', function(error, response, body) {
         if(!error && response.statusCode == 200) {
