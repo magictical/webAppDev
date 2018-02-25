e61bec0630ca36814267ed362eaf39eb1d0e2873
Ian Kim <magicticaldev@gmail.com>
2018-02-25 08:32:54 +0000

    Add campground.ejs, Add link each of page(landing, campground)and var campgrounds.

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -8,6 +8,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 })
 
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Bumusa", image: "https://farm3.staticflickr.com/2222/5763171257_b604848409.jpg"},
+        {name: "Nawansa", image: "https://farm7.staticflickr.com/6009/6189111529_5b70b82033.jpg"},
+        {name: "Sinan", image: "https://farm8.staticflickr.com/7254/13876685593_19feb04904.jpg"}
+    ]
+    res.render("campgrounds");
+})
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server has started!!!");
 })
