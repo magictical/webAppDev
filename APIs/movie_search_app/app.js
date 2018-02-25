b327fadb89277280154554719a251231b2022c1c
Ian Kim <magicticaldev@gmail.com>
2018-02-25 04:56:00 +0000

    Use render() send parseTojson to results.ejs template and use forEach() to iterates the jsonData finally got a list of movie title with 'star'

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
 app.get("/results", function(req, res) {
     request('http://www.omdbapi.com/?s=star&apikey=thewdb', function(error, response, body) {
         if(!error && response.statusCode == 200) {
@@ -9,12 +11,10 @@ app.get("/results", function(req, res) {
         var parseToJson = JSON.parse(body);
         //drill down to the data
         //json은 대소문자 구분해야한다!
-        res.send(parseToJson["Search"][0]["Title"]);
+        res.render("results", {parseToJson:parseToJson});
         console.log(parseToJson["Search"][0]["Title"]);
         }
     });
-    
-    
 });
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("Movie App has started!!") ;
