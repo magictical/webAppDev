42fa2a90831863af23adaba1fac96434c1c83ac9
Ian Kim <magicticaldev@gmail.com>
2018-02-26 12:35:46 +0000

    Add cat obj for MongoDB's model

--- Database/cat.js
+++ Database/cat.js
@@ -1,4 +1,16 @@
 var mongoose = require("mongoose");
+//connect to the DB
+mongoose.connect("mongodb://localhost/cat_app");
+
+//modeling the cat obj
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    tomperament: String
+});
+
+//define "Cat" as obj name and catSchema as a schema
+var Cat = mongoose.model("Cat", catSchema);
 
 //adding a new cat to the DB
 
