f966c6e2e46ba92894996588031816e38a3bcda9
Ian Kim <magicticaldev@gmail.com>
2018-02-26 13:56:45 +0000

    Retrieve Cat Data From DB

--- Database/cat.js
+++ Database/cat.js
@@ -6,27 +6,35 @@ mongoose.connect("mongodb://localhost/cat_app");
 var catSchema = new mongoose.Schema({
     name: String,
     age: Number,
-    tomperament: String
+    temperament: String
 });
 
 //define "Cat" as obj name and catSchema as a schema
 var Cat = mongoose.model("Cat", catSchema);
 
 //adding a new cat to the DB
-var george = new Cat({
-    name: "Mrs.Norris",
-    age:110,
-    temperament:"Evil!"
-});
+// var george = new Cat({
+//     name: "Mrs.Norris",
+//     age:110,
+//     temperament:"Evil!"
+// });
 
-george.save(function(error, cat) {
-    if(error) {
-        console.log("something went wrong");
-    } else {
-        console.log("save succeed!!");
-        console.log(cat);
-        }
-});
+// george.save(function(error, cat) {
+//     if(error) {
+//         console.log("something went wrong");
+//     } else {
+//         console.log("save succeed!!");
+//         console.log(cat);
+//         }
+// });
 
 //retrieve all cats from the DB and console.log each one.
+Cat.find({}, function(error, cats) {
+    if(error) {
+        console.log("something wrorg")
+    } else {
+        console.log("this is the list of the cats!");
+        console.log(cats);
+    }
+});
 
