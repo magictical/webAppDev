bd9480c8da896336de3eb663e5e0cea61fe7eade
Ian Kim <magicticaldev@gmail.com>
2018-02-26 06:33:14 +0000

    get name, imgUrl from "/campground/new" and inflate on "/campground"

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -25,14 +25,16 @@ app.get("/campgrounds", function(req, res) {
 
 // add POST route
 app.post("/campgrounds", function(req, res) {
-    //get data from body
+    //get name and imgUrl new.ejs
     var name = req.body.name;
     var imgUrl = req.body.image;
     var newCampground = {name:name, image:imgUrl};
+    //update campground
     campgrounds.push(newCampground);
     res.redirect("/campgrounds");
 })
 
+// new.ejs로 연결 - 이름, 이미지 주소입력을 name으로 받아서 action으로 "/campground로 넘김"
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 }) 
