07208bf4adb8e34dd884560a0a4c4ffa94a49151
Ian Kim <magicticaldev@gmail.com>
2018-02-26 06:29:53 +0000

    Add campground obj(name,imgUrl) and inflate  on "/campgrounds"

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -7,24 +7,30 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+        {name: "Bumusa", image: "https://farm3.staticflickr.com/2222/5763171257_b604848409.jpg"},
+        {name: "Nawansa", image: "https://farm7.staticflickr.com/6009/6189111529_5b70b82033.jpg"},
+        {name: "Sinan", image: "https://farm8.staticflickr.com/7254/13876685593_19feb04904.jpg"}
+    ];
+
 
 app.get("/", function(req, res) {
     res.render("landing");
 })
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Bumusa", image: "https://farm3.staticflickr.com/2222/5763171257_b604848409.jpg"},
-        {name: "Nawansa", image: "https://farm7.staticflickr.com/6009/6189111529_5b70b82033.jpg"},
-        {name: "Sinan", image: "https://farm8.staticflickr.com/7254/13876685593_19feb04904.jpg"}
-    ]
     // send object to campgrounds.ejs template
     res.render("campgrounds", {campgrounds:campgrounds});
 })
 
 // add POST route
 app.post("/campgrounds", function(req, res) {
-    res.send("YOU are AT POST ROUTE");
+    //get data from body
+    var name = req.body.name;
+    var imgUrl = req.body.image;
+    var newCampground = {name:name, image:imgUrl};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new", function(req, res) {
