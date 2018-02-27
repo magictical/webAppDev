94a7413e49a76ff400ba7bb5c7147700145338e3
Ian Kim <magicticaldev@gmail.com>
2018-02-27 00:25:29 +0000

    Init Asocciations and Add user model to DB

---
+++ Associations/embed.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo");
+
+//User - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name : String
+});
+var User = mongoose.model("User", userSchema);
+
+
+//POST - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+var postModel = mongoose.model("Post", postSchema);
+
+var newUser = new User({
+    email: "charie@as.com",
+    name: "charie"
+});
+newUser.save(function(err, user) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(user);
+    }
+})
