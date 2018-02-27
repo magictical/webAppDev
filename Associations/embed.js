941652fe74c93dc7e6197e26c3a0f71c1d2c01a0
Ian Kim <magicticaldev@gmail.com>
2018-02-27 00:51:02 +0000

    Define relationship btw User-Post : 1-N

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,20 +1,22 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
-//User - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name : String
-});
-var User = mongoose.model("User", userSchema);
-
-
 //POST - title, content
+//User에 포함된 관계일땐 미리 정의하고 userSchema에서 사용되어야 한다.
 var postSchema = new mongoose.Schema({
     title: String,
     content: String
 });
 var Post = mongoose.model("Post", postSchema);
+
+//User - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name : String,
+    posts: [postSchema]
+});
+var User = mongoose.model("User", userSchema);
+
 
 var newUser = new User({
     email: "charie@as.com",
@@ -42,3 +44,5 @@ newPost.save(function(err, post) {
         console.log(post);
     }
 });
+
+//1-N relationship
