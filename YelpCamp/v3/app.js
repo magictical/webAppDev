34a2e809795762d56491b5b8422e0277829630d2
Ian Kim <magicticaldev@gmail.com>
2018-02-27 06:08:26 +0000

    remove Campground from app.js but import it from campground.js

--- YelpCamp/v3/app.js
+++ YelpCamp/v3/app.js
@@ -3,20 +3,14 @@ var express    = require("express"),
 // use body-parser
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose");
+    Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 // body-parser setup
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //     {name: "Nawansa", 
---
+++ YelpCamp/v3/models/campground.js
@@ -0,0 +1,10 @@
+//setup mongoose
+var mongoose = require("mongoose");
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
