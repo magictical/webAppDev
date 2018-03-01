61629de908429237965505e5c7c01e641b7a7541
Ian Kim <magicticaldev@gmail.com>
2018-03-01 03:07:10 +0000

    Modify the comment longer and reallistic

--- YelpCamp/v5/seeds.js
+++ YelpCamp/v5/seeds.js
@@ -7,17 +7,17 @@ var data =[
     {
         name: "deserted space",
         image: "https://d2r2jvvtffo57h.cloudfront.net/assets/img/strains/SnowDome-live_09e84188f2d857bb8f54b3a2af489dd34b96ea87d21db102632638be5e3cac9c.jpg",
-        description: "you should see this rfor real!!"        
+        description: "Bacon ipsum dolor amet bacon buffalo chicken, andouille brisket beef ribs bresaola drumstick pork pancetta filet mignon ham. Landjaeger t-bone pork, porchetta ham hock bresaola shank rump. Sirloin pork loin pancetta pork chop meatball buffalo t-bone cupim burgdoggen shank. Tenderloin salami bacon pork chop, swine meatloaf filet mignon. Chuck shoulder tenderloin spare ribs."        
     },
     {
         name: "honey space",
         image: "http://www.outdoornews.co.kr/news/photo/201105/2026_5666_4043.jpg",
-        description: "you should see this rfor real!!"        
+        description: "Bacon ipsum dolor amet bacon buffalo chicken, andouille brisket beef ribs bresaola drumstick pork pancetta filet mignon ham. Landjaeger t-bone pork, porchetta ham hock bresaola shank rump. Sirloin pork loin pancetta pork chop meatball buffalo t-bone cupim burgdoggen shank. Tenderloin salami bacon pork chop, swine meatloaf filet mignon. Chuck shoulder tenderloin spare ribs.!"        
     },
     {
         name: "my space",
         image: "http://www.mikebartonphoto.com/images/large/CUBoulderFlatirons2.jpg",
-        description: "you can do this!!"        
+        description: "Bacon ipsum dolor amet bacon buffalo chicken, andouille brisket beef ribs bresaola drumstick pork pancetta filet mignon ham. Landjaeger t-bone pork, porchetta ham hock bresaola shank rump. Sirloin pork loin pancetta pork chop meatball buffalo t-bone cupim burgdoggen shank. Tenderloin salami bacon pork chop, swine meatloaf filet mignon. Chuck shoulder tenderloin spare ribs."        
     }
 ]
 
