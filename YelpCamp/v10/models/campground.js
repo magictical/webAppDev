3a354d0dd8276c4601b085ee190287de45a4e261
Ian Kim <magicticaldev@gmail.com>
2018-03-07 09:14:56 +0000

    Copy folders from v9 to v10

---
+++ YelpCamp/v10/models/campground.js
@@ -0,0 +1,23 @@
+//setup mongoose
+var mongoose = require("mongoose");
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    author: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        username:String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.Object,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
