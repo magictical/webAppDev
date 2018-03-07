e08a0eea4eb7754cd56104983349c2c78b44d601
Ian Kim <magicticaldev@gmail.com>
2018-03-07 03:31:31 +0000

    Save User obj info inside the comment obj

--- YelpCamp/v8/models/comment.js
+++ YelpCamp/v8/models/comment.js
@@ -2,7 +2,14 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
     text: String,
-    author: String
+    author: {
+        //save User data
+        id: {
+            type: mongoose.Schema.Type.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
