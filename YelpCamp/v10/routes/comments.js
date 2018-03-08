9e8f1dac08a9a3980fc0d4a832ed1464bcf41ed3
Ian Kim <magicticaldev@gmail.com>
2018-03-08 04:29:36 +0000

    Add middleware to check Author on comment

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -53,7 +53,7 @@ router.post("/", isLogedIn, function(req, res) {
 });
 
 // EditCommentRoute
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             console.log(err);
@@ -67,7 +67,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // COMMENT UPDATE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
     //find and update the correct campground
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
       if(err) {
@@ -80,7 +80,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // COMMENT DESTROY ROUTE
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
     //findByIdAndRemove
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
@@ -101,6 +101,29 @@ function isLogedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+//check authorization
+function checkCommentOwnership(req, res, next) {
+    //check logged in?
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                console.log(err);
+                res.redirect("back");
+            } else {
+                //is user own the campground?
+                console.log(foundComment.author.id)
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();   
+                } else {
+                    res.redirect("back");            
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
