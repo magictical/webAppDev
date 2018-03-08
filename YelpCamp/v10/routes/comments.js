2fc43ac897682678220722c6f6967cba6154db00
Ian Kim <magicticaldev@gmail.com>
2018-03-08 03:04:22 +0000

    Add UPATE - comment route

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -62,9 +62,23 @@ router.get("/:comment_id/edit", function(req, res) {
             console.log(req.params.id);
             console.log(foundComment);
         }
-    })
+    });
     
-})
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", function(req, res) {
+    //find and update the correct campground
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+      if(err) {
+          console.log(err);
+          res.redirect("back");
+      }  else {
+          res.redirect("/campgrounds/" + req.params.id);
+      }
+    });
+});
+
 
 //define middleware
 function isLogedIn(req, res, next) {
